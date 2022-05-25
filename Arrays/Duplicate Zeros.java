b1f1f1a437b3e50d1fca3325c6881cbcf0d8ae22
Aradhya <aradhyamahant@gmail.com>
2022-05-25 09:27:16 +0530

    double the no. of zeros in array

---
+++ Arrays/Duplicate Zeros.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int l = arr.length;
+        for(int i = 0;i< l-1 ;i++){
+            if(arr[i] == 0){
+                for(int j = l-2; j >= i ; j--){
+                    arr[j+1] = arr[j];
+                }
+                i++;
+            }
+        }
+        
+    }
+}
